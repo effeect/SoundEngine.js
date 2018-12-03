87d362028acda350d68cc38027232f77c1937d7b
effeect <oliver.dimes3@gmail.com>
2018-12-03 16:53:05 +0000

    Template File

--- sketch.js
+++ sketch.js
@@ -1,12 +1,30 @@
+/*
+Hello there!
+
+This a template sketch file that you can use to start creating using the power of soundEngine.
+
+There are five main variables you can access that give out values, they spit out values between 0 and 255 :
+    - bass
+    - lowMid
+    - mid
+    - highMid
+    - treble
+    - level (from 0 to 1)
+
+For future reference, the only functions you need for this to function is use "setupSound" in setup and "drawSound" in setup!
+
+If you want examples please look in the examples folder included.
+
+Thanks,
+effeect
+*/
+
 function setup()
 {
-    createCanvas(512,512)
     setupSound()
 }
 
 function draw()
 {
-    soundDraw()
-    background(0)
-    ellipse(256,256,bass,bass)
+    drawSound()
 }
